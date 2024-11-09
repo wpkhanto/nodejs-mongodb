e3084c0db7489e30806b0d2b24e8022abce28c3e
Watcharaphon Khanto <wpkhanto@icloud.com>
2024-11-09 15:18:55 +0700

    A better file structure 'routes'

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 // 1) MIDDLEWARES
-
 app.use(morgan('dev')); // tiny or dev
 
 app.use(express.json());
@@ -20,152 +21,11 @@ app.use((req, res, next) => {
     next();
 });
 
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
-);
-
-// 2) ROUTE HANDLERS
-const getAllTours = (req, res) => {
-    console.log(req.requestTime);
-
-    res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        results: tours.length,
-        data: {
-            tours,
-        },
-    });
-};
-
-const getTour = (req, res) => {
-    console.log(req.params);
-    const id = req.params.id * 1;
-
-    const tour = tours.find((el) => el.id === id);
-
-    // if (id > tours.length) {
-    if (!tour) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID',
-        });
-    }
-
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tour,
-        },
-    });
-};
-
-const createTour = (req, res) => {
-    // console.log(req.body);
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign(
-        ...{
-            id: newId,
-        },
-        ...req.body,
-    );
-
-    tours.push(newTour);
-
-    fs.writeFile(
-        `${__dirname}/dev-data/data/tours-simple.json`,
-        JSON.stringify(tours),
-        () => {
-            res.status(201).json({
-                status: 'success',
-                data: {
-                    tour: newTour,
-                },
-            });
-        },
-    );
-};
-
-const updateTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID',
-        });
-    }
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<Update tour here...>',
-        },
-    });
-};
-
-const deleteTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID',
-        });
-    }
-    res.status(204).json({
-        status: 'success',
-        data: null,
-    });
-};
-
-const getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!',
-    });
-};
-
-const getUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!',
-    });
-};
-
-const createUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!',
-    });
-};
-
-const updateUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!',
-    });
-};
-
-const deleteUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!',
-    });
-};
-
-// 3) ROUTES
-const tourRouter = express.Router();
-const userRouter = express.Router();
-
-// tours routes.
-tourRouter.route('/').get(getAllTours).post(createTour);
-tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
-
-// users routes.
-userRouter.route('/').get(getAllUsers).post(createUser);
-userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
-
+// 2) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// 4) START SERVER
+// 3) START SERVER
 const port = 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
